34e846e302949bf969ce72d70ac2b0ccd058178b
Deekshith H R <58986536+Deekshit7483@users.noreply.github.com>
2023-12-31 11:06:08 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA9xVi6g9Qo86cvtWJMptBfvAobCe1oRko",
+    authDomain: "logindemo-7a961.firebaseapp.com",
+    projectId: "logindemo-7a961",
+    storageBucket: "logindemo-7a961.appspot.com",
+    messagingSenderId: "616629456155",
+    appId: "1:616629456155:web:134094c4ea065f6b1797be",
+    measurementId: "G-F20767JHKE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
